2c0964ffc9a84fc5b247fea5d1fbc5cdaa7806f7
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-02-27 16:33:12 -0300

    feat: Inserting validation

--- src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.diogooliveira.vacancy_management.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data // Criando os getters e setters para todos os atributos para CandidateEntity
@@ -9,8 +13,14 @@ public class CandidateEntity { // Informações do candidato
     
     private UUID id;
     private String name;
+
+    @Pattern(regexp =  "^(?!\\s*$).+", message =  "The 'username' field should not contain spaces.")
     private String username;
+
+    @Email(message = "The 'email' field must contain a valid email.")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.diogooliveira.vacancy_management.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
     @PostMapping("/")
-    public void create( @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
+    public void create( @Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidate");
         System.out.println(candidateEntity.getEmail());
     }
     
