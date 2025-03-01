94065875137c25bf4ee191c8fe16b4d87e0410e8
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-03-01 12:24:49 -0300

    feat: Job Controller

--- src/main/java/br/com/diogooliveira/vacancy_management/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/diogooliveira/vacancy_management/modules/company/entities/JobEntity.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -22,13 +23,15 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+
+    @NotBlank(message = "This field is required.")
     private String level;
 
     @ManyToOne()
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
-    @Column(name = "company_id")
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
 
     @CreationTimestamp
