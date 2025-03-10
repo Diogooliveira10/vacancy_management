92ca12050f69632e84cd3dba28b44820a361db81
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-03-10 15:57:47 -0300

    feat: Use of tokens and security filters

--- src/main/java/br/com/diogooliveira/vacancy_management/security/SecurityFilter.java
+++ src/main/java/br/com/diogooliveira/vacancy_management/security/SecurityFilter.java
@@ -27,8 +27,9 @@ public class SecurityFilter extends OncePerRequestFilter {
 
         SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
-        
-        if (header != null) {
+
+        if (request.getRequestURI().startsWith("/company")) {
+          if (header != null) {
             var subjectToken = this.jwtProvider.validateToken(header);
             if (subjectToken.isEmpty()) {
               response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -40,7 +41,8 @@ public class SecurityFilter extends OncePerRequestFilter {
                 Collections.emptyList());
       
             SecurityContextHolder.getContext().setAuthentication(auth);
-          }      
+          }
+        }      
         
         filterChain.doFilter(request, response);
     }
