964b8436b7f9811f1e181bf27ef7e2aa68296473
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-03-12 11:29:54 -0300

    feat: UseCase: ListAllJobsByFilter

--- src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.diogooliveira.vacancy_management.modules.candidate.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.diogooliveira.vacancy_management.modules.candidate.CandidateEntity;
 import br.com.diogooliveira.vacancy_management.modules.candidate.userCases.CreateCandidateUseCase;
+import br.com.diogooliveira.vacancy_management.modules.candidate.userCases.ListAllJobsByFilterUseCase;
 import br.com.diogooliveira.vacancy_management.modules.candidate.userCases.ProfileCandidateUseCase;
+import br.com.diogooliveira.vacancy_management.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -26,6 +30,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping("/")
     public ResponseEntity<Object> create( @Valid @RequestBody CandidateEntity candidateEntity) {
@@ -52,4 +59,10 @@ public class CandidateController {
         }
     }
     
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
+    }
 }
---
+++ src/main/java/br/com/diogooliveira/vacancy_management/modules/candidate/userCases/ListAllJobsByFilterUseCase.java
@@ -0,0 +1,20 @@
+package br.com.diogooliveira.vacancy_management.modules.candidate.userCases;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.diogooliveira.vacancy_management.modules.company.entities.JobEntity;
+import br.com.diogooliveira.vacancy_management.modules.company.repositories.JobRepository;
+
+@Service
+public class ListAllJobsByFilterUseCase {
+
+    @Autowired
+    private JobRepository jogRepository;
+    
+    public List<JobEntity> execute(String filter) {
+        return this.jogRepository.findByDescriptionContaining(filter);
+    }
+}
